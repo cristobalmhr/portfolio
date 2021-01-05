45f1bf78a79c9043536cb9672f4e490e81a717fa
Cristobal Martinez <cristobalmartinezhijar@hotmail.com>
2021-01-04 19:20:08 -0600

    ➕ ADD: Color palette for material ui

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,53 +6,49 @@ import Portfolio from '../containers/Portfolio'
 import Resume from '../containers/Resume'
 import Contact from '../containers/Contact'
 import Project from '../containers/Project'
+import Layout from '../components/Layout'
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5E7C51',
+    },
+    secondary: {
+      main: '#5f4109',
+    },
+  },
+})
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link to="/project">Project</Link>
-            </li>
-            <li>
-              <Link to="/resume">Resume</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
+    <ThemeProvider theme={theme}>
+      <Router>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL.
             So, the path '/' must go to the end of the switch */}
-        <Switch>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/project">
-            <Project />
-          </Route>
-          <Route path="/resume">
-            <Resume />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+        <Layout>
+          <Switch>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/project">
+              <Project />
+            </Route>
+            <Route path="/resume">
+              <Resume />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   )
 }
 
