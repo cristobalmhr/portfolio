2a0ae4614c53d1cfd764f52aec8266704008fb6f
Cristobal Martinez <cristobalmartinezhijar@hotmail.com>
2021-01-06 14:38:13 -0600

    ➕ ADD: First implementation of dynamic themes for dark mode in desktop toolbar

--- src/components/ToolbarDesktop.jsx
+++ src/components/ToolbarDesktop.jsx
@@ -1,40 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Toolbar, IconButton } from '@material-ui/core'
 import NightsStayOutlinedIcon from '@material-ui/icons/NightsStayOutlined'
+import Brightness5OutlinedIcon from '@material-ui/icons/Brightness5Outlined'
 
-import logo from '../resources/images/logo-lg.PNG'
+import logoLight from '../resources/images/logo-lg-t.png'
+import logoDark from '../resources/images/logo-lg-t-d.png'
+
 import MenuButton from './MenuButton'
 import '../styles/components/ToolbarDesktop.css'
 import { connect } from 'react-redux'
-
-// const menuPages = [
-//   {
-//     name: 'Home',
-//     path: '/home',
-//     active: true,
-//   },
-//   {
-//     name: 'Portfolio',
-//     path: '/portfolio',
-//     active: false,
-//   },
-//   {
-//     name: 'Resume',
-//     path: '/resume',
-//     active: false,
-//   },
-//   {
-//     name: 'Contact',
-//     path: '/contact',
-//     active: false,
-//   },
-// ]
+import { useMediaQuery } from 'react-responsive'
 
 const ToolbarDesktop = ({ menuPages }) => {
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: '(prefers-color-scheme: dark)',
+    },
+    undefined,
+    (prefersDark) => {
+      setIsDark(prefersDark)
+    }
+  )
+
+  const [isDark, setIsDark] = useState(systemPrefersDark)
+  const DARK_CLASS = 'dark'
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add(DARK_CLASS)
+    } else {
+      document.documentElement.classList.remove(DARK_CLASS)
+    }
+  }, [isDark])
+
   return (
-    <Toolbar>
+    <Toolbar className="toolbar">
       <div className="header-container">
-        <img src={logo} alt="Logo" width={270} className="logo-large" />
+        <img
+          src={isDark ? logoDark : logoLight}
+          alt="Logo"
+          width={270}
+          className="logo-large"
+        />
 
         <div className="menu-container">
           {menuPages.map((item) => (
@@ -48,27 +55,29 @@ const ToolbarDesktop = ({ menuPages }) => {
             </div>
           ))}
 
-          {/* <IconButton
+          {isDark ? (
+            <IconButton
               edge="start"
               color="inherit"
               aria-label="menu"
               aria-haspopup="true"
-              className={iconButton}
-              onClick={() => setDrawerOpen(true)}
+              className="icon-button"
+              onClick={() => setIsDark(false)}
             >
               <Brightness5OutlinedIcon />
-            </IconButton> */}
-
-          <IconButton
-            edge="start"
-            color="primary"
-            aria-label="menu"
-            aria-haspopup="true"
-            className="icon-button"
-            onClick={() => console.log('Change theme')}
-          >
-            <NightsStayOutlinedIcon />
-          </IconButton>
+            </IconButton>
+          ) : (
+            <IconButton
+              edge="start"
+              color="primary"
+              aria-label="menu"
+              aria-haspopup="true"
+              className="icon-button"
+              onClick={() => setIsDark(true)}
+            >
+              <NightsStayOutlinedIcon />
+            </IconButton>
+          )}
         </div>
       </div>
     </Toolbar>
